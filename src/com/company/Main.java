d0ca76350b275b1580b5438e24239880e0813867
Danila <EL_dranick@mail.ru>
2020-10-14 19:47:56 +0300

    мое

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-                int yearOfBirthday = 2000;
-                int monthOfBirthday = 6;
-                int dayOfBirthday = 6;
-                int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
-                System.out.println(sum + " Сумма из года, месяца и дня моего рождения");
+        int yearOfBirthday = 2000;
+        int monthOfBirthday = 6;
+        int dayOfBirthday = 6;
+        int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
+        System.out.println(sum + " Сумма из года, месяца и дня моего рождения");
 
-                boolean monthBiggerday = monthOfBirthday > dayOfBirthday;
-                System.out.println(monthBiggerday);
+        boolean monthBiggerday = monthOfBirthday > dayOfBirthday;
+        System.out.println(monthBiggerday);
 
-                char name[] = new char[] {'Д', 'а', 'н', 'и', 'л', 'а'};
-        System.out.println("Массив с моим именем: " + name[0] + name[1] + name[2] + name[3] + name[4] + name[5]);
-
-                double myAge = 20.0;
-                double partAge = 4.0 / 12.0;
-                double sumAge = myAge + partAge;
-        System.out.println("Мне " + sumAge + " лет");
-            }
+        char name[] = new char[]{'Д', 'а', 'н', 'и', 'л', 'а'};
+        System.out.print("Массив с моим именем: ");
+        for (int i = 0; i < name.length; i++) {
+            System.out.print(name[i]);
         }
+        System.out.println();
+
+        double myAge = 20.0;
+        double partAge = 4.0 / 12.0;
+        double sumAge = myAge + partAge;
+        System.out.println(" Мне " + sumAge + " лет");
+    }
+}
